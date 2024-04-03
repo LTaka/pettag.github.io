23d5a8ea9dfb24c08dd35c8681649ae6b29d7ad1
Ltaka <lucas.takeshi07@gmail.com>
2024-04-03 16:38:32 -0300

    primeira tela

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ProfileCard from './ProfileCard'; // Importando o componente
+
+function App() {
+  return (
+    <div className="App">
+      <ProfileCard />
+    </div>
+  );
+}
+
+export default App;

85c9de941670d82c9f04f201c3d5ef0c063d09ef
Lucas Takeshi <42739082+LTaka@users.noreply.github.com>
2024-04-03 23:45:55 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/pettag.github.io",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
